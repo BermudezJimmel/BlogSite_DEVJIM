637aae0dd7816cce885102eec084df51376c56ce
Jimmel Bermudez <bermudezjimmel7@gmail.com>
2021-01-14 16:22:46 +0800

    Adding Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
 
 function App() {
- const title = 'Welcome to the new Blog';
- const like = 50;
- // const person = { name: 'yoshi', age: 30};
- const link = "http://www.google.com";
-
-
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { like } times</p>
-      {/*  <p>{ person }</p> */}
-
-      <p> { 10 } </p>
-      <p> { "Hello DevJim"} </p>
-      <p> { [1,2,3,4,5] }</p>
-      <p>{ Math.random() *10 }</p>
-
-      <a href={link}>Google Site</a>
+        <h1>App Component</h1>
       </div>
     </div>
   );
